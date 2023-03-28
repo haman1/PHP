35382988436cdb622b23cb344466682d818d50b1
ombasa <ombasaherman@gmail.com>
2023-03-28 21:54:42 +0300

    set up

--- simpleblog/resources/views/home.blade.php
+++ simpleblog/resources/views/home.blade.php
@@ -1,12 +1,8 @@
 @extends('layout')
 
-
 @section('title', 'Home')
 
 @section('content')
-<h1>Home Page</h1>
-<p>This is the Home Page.</p>
+<h1>Home page</h1>
+<p>This is the home page.</p>
 @endsection
-
-
-
--- simpleblog/routes/web.php
+++ simpleblog/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('home');
 })->name('home');
@@ -11,3 +10,11 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+Route::get('/posts/create', function () {
+    return view('create');
+})->name('posts.create');
+
+Route::post('/posts', function () {
+    return redirect()
+        ->route('posts.create');
+})->name('posts.store');
