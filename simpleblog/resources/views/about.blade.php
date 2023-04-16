1fa1ab432411f990e23898067d3203ae1189543d
Ombasa Herman <ombasaherman@gmail.com>
2023-04-16 23:20:59 +0300

    Delete simpleblog directory

--- simpleblog/resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('title', 'About us')
-
-@section('content')
-<h1>About page</h1>
-<p>This is the about page.</p>
-@endsection
--- simpleblog/resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('title', 'Home')
-
-@section('content')
-<h1>Home page</h1>
-<p>This is the home page.</p>
-@endsection
